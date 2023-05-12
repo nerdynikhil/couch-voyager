dbf1f0e6273985cc90a97df8c6b9ea596a605d19
Nikhil Barik <nklmarch17@gmail.com>
2023-05-13 03:40:05 +0530

    feat: Add new aesthetic changes

--- popup.js
+++ popup.js
@@ -2,15 +2,25 @@
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     var url = tabs[0].url;
 
-    // Parse the location from the URL
-    var parser = document.createElement("a");
-    parser.href = url;
-    var location = parser.pathname;
+    // Check if the URL is for Google Maps
+    if (url.startsWith("https://www.google.com/maps/")) {
+        // Parse the location from the URL
+        var parser = document.createElement("a");
+        parser.href = url;
+        var location = parser.pathname;
 
-    // Find and display the location name
-    chrome.tabs.executeScript(tabs[0].id, {code: 'document.querySelector(".fontBodySmall").textContent'}, function (result) {
-        var locationName = result[0];
+        // Find and display the location name
+        chrome.tabs.executeScript(tabs[0].id, {code: 'document.querySelector(".fontBodySmall").textContent'}, function (result) {
+            var locationName = result[0];
+            var locationElement = document.getElementById("location");
+            locationElement.textContent = "You are in " + locationName + " at " + location;
+        });
+
+        // Display the quirky message
+        var quirkyMessageElement = document.getElementById("quirky-message");
+        quirkyMessageElement.textContent = "Time to explore the couches of the world!";
+    } else {
         var locationElement = document.getElementById("location");
-        locationElement.textContent = "You are in " + locationName + " at " + location;
-    });
+        locationElement.textContent = "Your couch is the best seat in the house! But to use this extension, you'll need to open Google Maps first";
+    }
 });
