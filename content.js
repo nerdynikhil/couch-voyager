bdbd2b90fee555072803c433e14de839b5155454
Rajana Vara Rahul <rajanarahul93@gmail.com>
2024-10-04 16:08:34 +0530

    Added auto-click functionality to click center of the page

--- content.js
+++ content.js
@@ -1,19 +1,35 @@
+function clickCenterOfPage() {
+  var x = window.innerWidth / 2;
+  var y = window.innerHeight / 2;
+
+  var event = new MouseEvent("click", {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+      clientX: x,
+      clientY: y
+  });
+
+  document.dispatchEvent(event);
+}
+
 function clickForwardButton() {
-    var forwardButton = document.getElementsByClassName("widget-scene-navigation-next")[0];
-    if (forwardButton) {
+  var forwardButton = document.getElementsByClassName("widget-scene-navigation-next")[0];
+  if (forwardButton) {
       var rect = forwardButton.getBoundingClientRect();
       var x = rect.left + rect.width / 2;
       var y = rect.top + rect.height / 2;
       var event = new MouseEvent("click", {
-        view: window,
-        bubbles: true,
-        cancelable: true,
-        clientX: x,
-        clientY: y
+          view: window,
+          bubbles: true,
+          cancelable: true,
+          clientX: x,
+          clientY: y
       });
       forwardButton.dispatchEvent(event);
-    }
+  } else {
+      clickCenterOfPage();
   }
-  
-  setInterval(clickForwardButton, 5000);
-  
+}
+
+setInterval(clickForwardButton, 5000);  // Click every 5 seconds
