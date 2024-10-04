94c82127d830c4986a207e5b788c5150a58f6c83
unknown <paliwalkusahgra07.com>
2024-10-04 11:23:38 +0530

    new code

--- content.js
+++ content.js
@@ -1,19 +1,19 @@
 function clickForwardButton() {
-    var forwardButton = document.getElementsByClassName("widget-scene-navigation-next")[0];
-    if (forwardButton) {
+  var forwardButton = document.getElementsByClassName("widget-scene-navigation-next")[0];
+  if (forwardButton) {
       var rect = forwardButton.getBoundingClientRect();
       var x = rect.left + rect.width / 2;
       var y = rect.top + rect.height / 2;
       var event = new MouseEvent("click", {
-        view: window,
-        bubbles: true,
-        cancelable: true,
-        clientX: x,
-        clientY: y
+          view: window,
+          bubbles: true,
+          cancelable: true,
+          clientX: x,
+          clientY: y
       });
       forwardButton.dispatchEvent(event);
-    }
   }
-  
-  setInterval(clickForwardButton, 5000);
-  
+}
+
+// Set an interval to auto-click the forward button every 5 seconds
+setInterval(clickForwardButton, 5000);
--- popup.js
+++ popup.js
@@ -1,35 +1,35 @@
 // Get the current tab's URL
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    var url = tabs[0].url;
+  var url = tabs[0].url;
 
-    // Check if the URL is for Google Maps
-    if (url.startsWith("https://www.google.com/maps/")) {
-        // Parse the location from the URL
-        var parser = document.createElement("a");
-        parser.href = url;
-        var location = parser.pathname;
+  // Check if the URL is for Google Maps
+  if (url.startsWith("https://www.google.com/maps/")) {
+      // Parse the location from the URL
+      var parser = document.createElement("a");
+      parser.href = url;
+      var location = parser.pathname;
 
-        // Find and display the location name
-        chrome.tabs.executeScript(tabs[0].id, {code: 'document.querySelector(".fontBodySmall").textContent'}, function (result) {
-            var locationName = result[0];
-            var locationElement = document.getElementById("location");
-            locationElement.textContent = "You are in " + locationName + " at " + location;
-        });
+      // Find and display the location name
+      chrome.tabs.executeScript(tabs[0].id, { code: 'document.querySelector(".fontBodySmall").textContent' }, function (result) {
+          var locationName = result[0];
+          var locationElement = document.getElementById("location");
+          locationElement.textContent = "You are in " + locationName + " at " + location;
+      });
 
-        // Display the quirky message
-        var quirkyMessageElement = document.getElementById("quirky-message");
-        quirkyMessageElement.textContent = "Time to explore the couches of the world!";
-    } else {
-        var locationElement = document.getElementById("location");
-        locationElement.textContent = "Your couch is the best seat in the house! But to use this extension, you'll need to open Google Maps first";
-    }
+      // Display the quirky message
+      var quirkyMessageElement = document.getElementById("quirky-message");
+      quirkyMessageElement.textContent = "Time to explore the couches of the world!";
+  } else {
+      var locationElement = document.getElementById("location");
+      locationElement.textContent = "Your couch is the best seat in the house! But to use this extension, you'll need to open Google Maps first";
+  }
 });
 
 // Get the "Let's go" button element
 var letsGoButton = document.getElementById("lets-go-button");
 
 // Add a click event listener to the button
-letsGoButton.addEventListener("click", function() {
+letsGoButton.addEventListener("click", function () {
   // Find the canvas element
   var canvas = document.querySelector(".widget-scene-canvas");
 
@@ -38,11 +38,32 @@ letsGoButton.addEventListener("click", function() {
   var x = rect.left + rect.width / 2;
   var y = rect.top + rect.height / 2;
   var event = new MouseEvent("click", {
-    view: window,
-    bubbles: true,
-    cancelable: true,
-    clientX: x,
-    clientY: y
+      view: window,
+      bubbles: true,
+      cancelable: true,
+      clientX: x,
+      clientY: y
   });
   canvas.dispatchEvent(event);
 });
+
+// Function to auto-click the center of the page
+function clickCenterOfPage() {
+  var body = document.body; // Target the body or the main container element
+  var rect = body.getBoundingClientRect();
+  var x = rect.left + rect.width / 2;
+  var y = rect.top + rect.height / 2;
+
+  var event = new MouseEvent("click", {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+      clientX: x,
+      clientY: y
+  });
+
+  body.dispatchEvent(event); // Simulate the click event
+}
+
+// Set an interval to auto-click the center of the page every 5 seconds
+setInterval(clickCenterOfPage, 5000);
